a203a764e5cade0bbeff329bb10d970fce081b0c
XavierCTD <macfamlove01@gmail.com>
2025-01-10 14:44:33 -0500

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const addFooter = document.body;
+const contentFooter = document.querySelector('#content');
 const useFooter = document.createElement('footer');
-footer.innerHTML = "This is a text footer";
-addFooter.appendChild(useFooter);
+useFooter.innerHTML = "<fieldset><label class='bio'>Message Here.<textarea id='bio' name='bio' rows='5' col='30'></textarea></label></fieldset>";
+useFooter.style.color = "darkblue";
+useFooter.style.textAlign = "center";
+useFooter.style.fontSize = "4rem";
+contentFooter.appendChild(useFooter);
+
+const today = new Date();
+const thisYear = today.getFullYear();
+const footer = document.querySelector('footer');
+const copyright = document.createElement('p');
+copyright.textContent = `Xavier Mcallister ${thisYear}.`;
+footer.appendChild(copyright);
