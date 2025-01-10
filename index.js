14ed879594aa1b4f05551990b1646c8521a964c5
XavierCTD <macfamlove01@gmail.com>
2025-01-10 12:28:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-const newPage = document.createElement("p");
-const textWritten = document.createTextNode("If you want more information on me, click here.")
-newPage.appendChild(textWritten);
 
-document.getElementByID("myfooter").appendChild(newPage);
