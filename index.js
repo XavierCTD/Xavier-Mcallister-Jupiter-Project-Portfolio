adf0d9947143460a4f444fc1fdb26c446274ac53
XavierCTD <macfamlove01@gmail.com>
2025-01-27 18:02:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,66 @@
 // Adding Footer
-
 const contentFooter = document.querySelector('#content');
-const useFooter = document.createElement('footer');
-useFooter.innerHTML = "<fieldset><label class='bio'>Message Here.<textarea id='bio' name='bio' rows='5' col='30'></textarea></label></fieldset>";
-useFooter.style.color = "darkblue";
-useFooter.style.textAlign = "center";
-useFooter.style.fontSize = "4rem";
-contentFooter.appendChild(useFooter);
+const footerElement = document.createElement('footer');
+footerElement.innerHTML = "<fieldset><label class='bio'>Message Here.<textarea id='bio' name='bio' rows='5' cols='30'></textarea></label></fieldset>";
+footerElement.style.color = "darkblue";
+footerElement.style.textAlign = "center";
+footerElement.style.fontSize = "4rem";
+if (contentFooter) contentFooter.appendChild(footerElement);
 
 // Adding Date
-
 const today = new Date();
 const thisYear = today.getFullYear();
 const footer = document.getElementById('timing');
-const copyright = document.createElement('p');
-copyright.textContent = `Xavier Mcallister ${thisYear}.`;
-footer.appendChild(copyright);
-
+if (footer) {
+  const copyright = document.createElement('p');
+  copyright.textContent = `Xavier Mcallister ${thisYear}.`;
+  footer.appendChild(copyright);
+}
 
 // Adding Skills
-
-const useSkills = ["JavaScript", "HTML", "CSS", "Very good with video games"];
+const skills = ["JavaScript", "HTML", "CSS", "Very good with video games"];
 const skillsSection = document.getElementById('skills-list');
-const skillsList = document.querySelector('ul');
-
-for(let i = 0; i < useSkills.length; i++) {
-  const skill = document.createElement('li');
-  skill.innerText = useSkills[i];
-  skillsList.appendChild(skill);
+const skillsList = document.querySelector('#skills-list ul');
+if (skillsList) {
+  skills.forEach(skill => {
+    const skillItem = document.createElement('li');
+    skillItem.innerText = skill;
+    skillsList.appendChild(skillItem);
+  });
 }
 
 // Adding a Message Form
-
 const messageForm = document.forms["leave_message"];
-messageForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  
-  const usersName = event.target.usersName.value;
-  const usersEmail = event.target.usersEmail.value;
-  const usersMessage = event.target.usersMessage.value;
+if (messageForm) {
+  messageForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  console.log("User's Name:", usersName);
-  console.log("User's Email:", usersEmail);
-  console.log("User's Message:", usersMessage);
+    const usersName = this.usersName.value;
+    const usersEmail = this.usersEmail.value;
+    const usersMessage = this.usersMessage.value;
 
-  const messageSection = document.getElementById('messages');
-  const messageList = messageSection.querySelector('ul');
-  const newMessage = document.createElement('li');
-  
-  newMessage.innerHTML = `<a href="mailto:${usersName}"> ${usersEmail}</a>
-  <span>${usersMessage}</span>`
+    console.log("User's Name:", usersName);
+    console.log("User's Email:", usersEmail);
+    console.log("User's Message:", usersMessage);
 
-  messageList.appendChild(newMessage);
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection?.querySelector('ul');
+    if (messageList) {
+      const newMessage = document.createElement('li');
+      newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a>
+      <span>${usersMessage}</span>`;
 
-  messageForm.reset();
+      const removeButton = document.createElement('button');
+      removeButton.innerText = 'remove';
+      removeButton.type = 'button';
+      removeButton.addEventListener("click", function () {
+        newMessage.remove();
+      });
 
-  const removeButton = document.createElement('button');
-  removeButton.innerText = 'remove';
-  removeButton.type = 'button';
-  removeButton.addEventListener("click", function (event) {
-    const entry = removeButton.parentNode;
-    entry.remove();
-  })
+      newMessage.appendChild(removeButton);
+      messageList.appendChild(newMessage);
+    }
 
-  newMessage.appendChild(removeButton);
-
-  messageList.appendChild(newMessage);
-
-  event.target.reset();
-});
+    this.reset();
+  });
+}
